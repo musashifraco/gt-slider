ab1913d6d53716e90ebb4943a047629dcfe32648
ezequiel.everton <ezequiel.everton@ceuma.br>
2024-06-26 19:12:42 -0300

    fix: pequenas alteracoes na estilizacao do componente Slider

--- src/components/Slider/Slider.styles.js
+++ src/components/Slider/Slider.styles.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 
 export const Container = styled.div`
-  //box-sizing: border-box;
+  // box-sizing: border-box;
   position: relative;
   display: flex;
   overflow-y: scroll;
